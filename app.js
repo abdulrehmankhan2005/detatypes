b710f89a56a6b1ac4e25e8cd2656e87a613e1dbf
abdulrehmankhan2005 <iamabdulrehmankhan900@gmail.com>
2024-10-18 01:01:25 -0700

    feat: app.js

---
+++ app.js
@@ -0,0 +1,51 @@
+// deta types
+// Primitive Data Types
+
+// String:
+
+// String:
+// let name = "John";
+// console.log(name); // Output: John
+// Number:
+// let age = 25;
+
+// let price = 99.99;
+
+// boolians:
+// let isStudent = true;
+
+// Undefined:
+// let myVar;
+// console.log(myVar); // Output: undefined
+
+// null:
+
+// let job = null;
+// console.log(job); // Output: null
+
+// BigInt:
+// let bigNumber = 123456789012345678901234567890n;
+// console.log(bigNumber); // Output: 123456789012345678901234567890n
+
+// Symbol:
+// let symbol1 = Symbol("id");
+// let symbol2 = Symbol("id");
+// console.log(symbol1 === symbol2); // Output: false (har symbol unique hota hai)
+
+// Non-Primitive Data Types (ya Reference Types)
+
+// Object
+
+// Key-value pairs store karta hai.
+// Example:
+// let person = { name: "Ali", age: 22 };
+// console.log(person.name); // Output: Ali
+
+
+// Array
+
+// Ordered list of values ko store karta hai.
+// Example:
+// let fruits = ["apple", "banana", "mango"];
+// console.log(fruits[1]); // Output: banana
+// String
